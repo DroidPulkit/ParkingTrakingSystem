ff458ee64a83356da9463aea82a6384baba129d2
droidpulkit <pulkitkumar190@gmail.com>
2018-03-03 18:54:47 -0500

    Added database to the Registration page

--- Parking Tracker/AppDelegate.swift
+++ Parking Tracker/AppDelegate.swift
@@ -14,7 +14,7 @@ import Firebase
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var storyboard: UIStoryboard?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         //This code below is used to transparent the navigation bar in whole app
--- Parking Tracker/registerVC.swift
+++ Parking Tracker/registerVC.swift
@@ -18,19 +18,27 @@ class registerVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource
     @IBOutlet weak var txtCarPlateNumber: UITextField!
     @IBOutlet weak var cityPicker: UIPickerView!
     
+    //This is the list for the picker
     var cityList: [String] = ["Vancouver", "Ottawa", "Toronto", "Calgary", "Windsor", "Ajax", "Pickering","Brampton", "Mississauga", "NorthYork", "Scarborough"]
     var selectedCityIndex: Int = 0
     
+    //Firebase stuff
+    var ref: DatabaseReference = Database.database().reference()
+    
+    
+    
+
+    
     override func viewWillAppear(_ animated: Bool) {
         self.navigationItem.title = "Register"
-        
+        //Adding button to navigation menu
         let btnSubmit = UIBarButtonItem(title: "Submit", style: .plain, target: self, action: #selector(displayValues))
         
         self.navigationItem.rightBarButtonItem = btnSubmit
-        
+        //Making the title white
         let textAttributes = [NSAttributedStringKey.foregroundColor:UIColor.white]
         navigationController?.navigationBar.titleTextAttributes = textAttributes
-        
+        //This makes the back button white
         navigationController?.navigationBar.tintColor = UIColor.white
     }
     
@@ -63,6 +71,7 @@ class registerVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource
     
     func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
         let titleData = cityList[row]
+        //Adding title to the pickerview and then adding attributes like making it white and adding custom font to it.
         return NSAttributedString(string: titleData, attributes: [NSAttributedStringKey.foregroundColor: UIColor.white, NSAttributedStringKey.font: UIFont.fontNames(forFamilyName: "Chalkboard SE")])
     }
     
@@ -70,6 +79,7 @@ class registerVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource
     @objc private func displayValues(){
         self.selectedCityIndex = self.cityPicker.selectedRow(inComponent: 0)
         
+        //Extracting values
         let name = self.txtName.text!
         let email = self.txtEmail.text!
         let pass = self.txtPassword.text!
@@ -79,6 +89,7 @@ class registerVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource
         
         let allData: String = "\(name) \n \(email) \n \(pass) \n \(number) \n \(city) \n \(carNo)"
         
+        //Validating the data
         if (name.isEmpty || email.isEmpty || pass.isEmpty || number.isEmpty || city.isEmpty || carNo.isEmpty){
             let infoAlert = UIAlertController(title: "Error", message: "Fields cannot be empty", preferredStyle: .alert)
             infoAlert.addAction(UIAlertAction(title: "Try Again", style: .destructive, handler: nil))
@@ -97,22 +108,36 @@ class registerVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource
         infoAlert.addAction(UIAlertAction(title: "Confirm", style: .default, handler: {_ in self.register()}))
         self.present(infoAlert, animated: true, completion: nil)
     }
-
+    
+    //Using firebase to register the data
     func register() {
-        //to do
+        //registering the user
         Auth.auth().createUser(withEmail: txtEmail.text!, password: txtPassword.text!) { (user, error) in
+            //This will work if there is some error, basically error handling by firebase
             if let error = error {
                 let infoAlert = UIAlertController(title: "Error", message: String(error.localizedDescription), preferredStyle: .alert)
                 infoAlert.addAction(UIAlertAction(title: "Try Again", style: .default, handler: nil))
                 self.present(infoAlert, animated: true, completion: nil)
                 return
             }
+            //If no error by firebase then this below will work
             let alert = UIAlertController(title: "Registeration", message: "User registered", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: {_ in self.backScreen()}))
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: {_ in self.saveToDatabase()}))
             self.present(alert, animated: true, completion: nil)
         }
     }
     
+    
+    func saveToDatabase() {
+        //This get's the user's id i.e. $userID
+        let userID = Auth.auth().currentUser?.uid
+        //userRef is reference to the /users/$userID
+        let userRef = self.ref.child("users").child(userID!)
+        //This line below adds the data of the user
+        userRef.setValue(["name" : self.txtName.text!, "number" : self.txtContactNumber.text!, "city" : self.cityList[selectedCityIndex], "car_number" : txtCarPlateNumber.text! ])
+        self.backScreen()
+    }
+    //Back screen
     func backScreen() {
         self.navigationController?.popViewController(animated: true)
     }
